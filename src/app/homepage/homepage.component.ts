0bdfd06465bf73da1003d4e1354554604773ba81
Rahul Ranjan <50379870+RahulGoku@users.noreply.github.com>
2021-08-30 23:01:03 +0530

    UI modify and logout added

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HelperService } from 'src/data/helper.service';
+import { Helper } from 'src/model/helper';
 
 @Component({
   selector: 'app-homepage',
@@ -6,12 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
- 
-  constructor() {
-    
-   }
+  useremail:any;
+  helperRegisterForm=new FormGroup({
+    'helperService':new FormControl(),
+    'location':new FormControl(),
+    'countrycode':new FormControl(),
+    'contact':new FormControl(),
+    'describe':new FormControl(),
+    'aadhar':new FormControl(),
+  });
+  helper=new Helper();
+    constructor(private hs:HelperService,private route: ActivatedRoute,private _router:Router) { }
+   
+    ngOnInit(): void {
+      this.route.queryParams.subscribe(params => {
+        this.useremail= params['param'];
+      // const userEmailID=this.hs.email;
+      // console.log("User Email ID : "+this.hs);
+      })
+      
+    }
+  
 
-  ngOnInit(): void {
+  
+
+  onSubmit(){
+    this.helper.helperService=this.helperRegisterForm.value.helperService;
+    this.helper.location=this.helperRegisterForm.value.location;
+    this.helper.countryCode=this.helperRegisterForm.value.countrycode;
+    this.helper.contact=this.helperRegisterForm.value.contact;
+    this.helper.aadhar=this.helperRegisterForm.value.aadhar;
+    this.helper.description=this.helperRegisterForm.value.describe;
+    this.helper.userEmail=this.useremail;
+
+    console.log(this.helperRegisterForm)
+     // helper object is ready create a sservice pass the object 
+        // to the service  - HelperService
+    this.hs.createHelper(this.helper)
+        .subscribe(
+          response=>{console.log(response)},
+          err=>{console.log(err)});
+          this.helperRegisterForm.reset();
+          alert("helper registered Successfully")
+          this._router.navigate(['/homepage'])
+          
   }
 
 }
